3bc80a34b804263a3283e2056010a33bc1713a62
shaked <sh5ked@gmail.com>
2026-07-02 19:41:53 +0300

    sharped-server

--- app.js
+++ app.js
@@ -5,25 +5,61 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 const express = require('express');
 const cors = require('cors');
 const connectDB = require("./config/db");
+const path = require('path');
 require('dotenv').config();
 
 const userRoutes = require("./routes/users");
+const { default: mongoose } = require('mongoose');
+const path = require('node:path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const allowedOrigins = [
+    "http://127.0.0.1:5501",
+    "http://localhost:3000",
+    process.env.RENDER_EXTERNAL_URL
+].filter(Boolean)
 
 app.use(express.json());
-app.use(cors({
-    origin: "http://127.0.0.1:5501"
-}));
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true)
+            } else {
+                callback(new Error("Not allowed by CORS policies"))
+            }
+        },
+    })
+);
 connectDB();
+
+if(!process.env.JWT_SECRET){
+    console.error("JWT_SECRET, is not defined in enviroment varaiables");
+    process.exit(1);        
+}
 
 app.get("/", (req, res) => {
     res.send('Welcome to our users managment app.');
 })
 
+app.get("/health", (req, res) => {
+ const dbStates = ["connected", "disconnected", "connecting", "disconnecting"];
+ const dbConnected = mongoose.connection.readyState === 1;
+
+ res.status(dbConnected ? 200 : 503).json({
+    status: dbConnected ? "ok" : "error",
+    db: dbStates[mongoose.connection.readyState],
+    runtime: `${Math.floor(process.uptime())}s`,    
+    environment: process.env.NODE_ENV || "development"
+ })
+});
+
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "public")));
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
-    
+
+
 });
